05091b223f8c5feed655a0816c30a22e91ebfbf6
Bogdan B <bogdan.v@morpheus.dnoc.info>
2023-03-24 14:47:59 +0200

    Fix transfers

--- airdrop.ts
+++ airdrop.ts
@@ -1,11 +1,13 @@
-import * as csv from 'csv-parser';
+import csv from 'csv-parser';
 import TronWeb from 'tronweb';
 import { createReadStream } from "fs";
 
+import * as secrets from "./secrets.json"
+
 async function sendTokens(csvFile: string, privateKey: string, tokenId: string) {
-    const fullNode = 'https://api.trongrid.io';
-    const solidityNode = 'https://api.trongrid.io';
-    const eventServer = 'https://api.trongrid.io';
+    const fullNode = 'https://api.nileex.io/';
+    const solidityNode = 'https://api.nileex.io';
+    const eventServer = 'https://event.nileex.io';
 
     const tronWeb = new TronWeb({
         fullNode,
@@ -15,17 +17,25 @@ async function sendTokens(csvFile: string, privateKey: string, tokenId: string) 
     });
 
     const defaultAddress = tronWeb.address.fromPrivateKey(privateKey);
-
+    console.log(privateKey, defaultAddress);
     createReadStream(csvFile)
         .pipe(csv())
         .on('data', async (row) => {
-            const recipient = row[0];
-            const amount = parseInt(row[1]);
+            console.log(row)
+            const recipient = row.address;
+            const amount = parseInt(row.balance);
 
             try {
-                const transaction = await tronWeb.transactionBuilder.sendToken(recipient, amount, tokenId);
-                const signedTransaction = await tronWeb.trx.sign(transaction);
-                const result = await tronWeb.trx.broadcast(signedTransaction);
+                let contract = await tronWeb.contract().at(tokenId);
+                // const transaction = await tronWeb.transactionBuilder.sendToken(recipient, amount, tokenId);
+                // const signedTransaction = await tronWeb.trx.sign(transaction);
+                // const result = await tronWeb.trx.broadcast(signedTransaction);
+
+                let result = await contract.transfer(
+                    recipient, //address _to
+                    amount   //amount
+                ).send().then(output => { console.log('- Output:', output, '\n'); });
+
                 console.log(`Transaction to ${recipient} for ${amount} tokens:`, result);
             } catch (e) {
                 console.log(`Error sending tokens to ${recipient}:`, e);
@@ -35,4 +45,4 @@ async function sendTokens(csvFile: string, privateKey: string, tokenId: string) 
 
 
 console.log("Starting airdrop...");
-sendTokens('', 'jey', 'token');
+sendTokens('./test.csv', (secrets as any).privateKey, 'TXLAQ63Xg1NAzckPwKHvzw7CSEmLMEqcdj');
